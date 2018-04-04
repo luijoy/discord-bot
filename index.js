95b17f06cb7f30e88bdefa07279936638483dcea
Lui Joy <lui.kohl@gmail.com>
2018-04-04 13:02:49 +0200

    Added dice roll functionality

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const commando = require('discord.js-commando');
+const bot = new commando.Client();
 
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
 
-bot.on('message', (message) => {
-    if(message.content == 'ping') {
+bot.login('NDMxMDM1MjQ5NDg5NjA4NzA0.DaY4Yw.eRKxMdLF3xOKpw6RR4r53US5wwo');
+
+// const Discord = require('discord.js');
+// const bot = new Discord.Client();
+
+/* bot.on('message', (message) => {
+    if (message.content == 'ping') {
         // message.reply('pong');
         message.channel.sendMessage('pong');
     }
-});
+}); */
 
-bot.login('NDMxMDM1MjQ5NDg5NjA4NzA0.DaY4Yw.eRKxMdLF3xOKpw6RR4r53US5wwo');
+
